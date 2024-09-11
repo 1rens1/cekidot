3f081c40655f5eaf1f7d77ccae0b374504c63ede
1rens1 <rein.satyawarman@gmail.com>
2024-09-12 05:45:31 +0700

    work 7.2

--- netlify/functions/screenshot/index.ts
+++ netlify/functions/screenshot/index.ts
@@ -1,6 +1,44 @@
-export async function handler() {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({ message: 'Hello world' })
-	};
-}
+import type { Handler } from '@netlify/functions';
+import chromium from 'chrome-aws-lambda';
+
+export const handler: Handler = async (event) => {
+	try {
+		const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
+
+		if (!pageToScreenshot)
+			return {
+				statusCode: 400,
+				body: JSON.stringify({ message: 'Page URL not defined' })
+			};
+
+		const browser = await chromium.puppeteer.launch({
+			args: chromium.args,
+			defaultViewport: chromium.defaultViewport,
+			executablePath: await chromium.executablePath,
+			headless: chromium.headless
+		});
+
+		const page = await browser.newPage();
+
+		await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+
+		const screenshot = await page.screenshot({ encoding: 'binary' });
+
+		await browser.close();
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: `Complete screenshot of ${pageToScreenshot}`,
+				buffer: screenshot
+			})
+		};
+	} catch (e) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				message: ''
+			})
+		};
+	}
+};
